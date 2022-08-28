1f08e29dd62110afe83e2f5bd071eee1a1fd6f8b
mikeson <mikeson.jnr@gmail.com>
2022-08-28 20:23:27 +0100

    playerSelection function created

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-
-function getComputerChoice(){
+function getComputerChoice() {
 
   let randomChoice = Math.floor(Math.random() * 3) + 1;
 
-  if (randomChoice === 1){
-    return "Rock";
-  } else if (randomChoice === 2){
-    return "Paper";
-  } else if (randomChoice === 3){
-    return "Scissors"
+  if (randomChoice === 1) {
+    return "rock";
+  } else if (randomChoice === 2) {
+    return "paper";
+  } else if (randomChoice === 3) {
+    return "scissors"
   }
 }
 
 console.log(getComputerChoice());
+
+
+function playerSelection(playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase()) {
+
+  if (playerChoice === "rock") {
+    return "rock";
+  } else if (playerChoice === "paper") {
+    return "paper";
+  } else if (playerChoice === "scissors") {
+    return "scissors";
+  } else {
+    alert("This choice is invalid!");
+    return playerSelection();
+  }
+
+}
+
+console.log(playerSelection());
