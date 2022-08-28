807cefd12b8a9f49a6c1a40da9a552f8f3750176
mikeson <mikeson.jnr@gmail.com>
2022-08-28 19:18:41 +0100

    Created getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+
+function getComputerChoice(){
+
+  let randomChoice = Math.floor(Math.random() * 3) + 1;
+
+  if (randomChoice === 1){
+    return "Rock";
+  } else if (randomChoice === 2){
+    return "Paper";
+  } else if (randomChoice === 3){
+    return "Scissors"
+  }
+}
+
+console.log(getComputerChoice());
